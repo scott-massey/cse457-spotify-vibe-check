5a879e49d05e9ea390c903a600f398c09cd4550b
Scott Massey <scott.massey@wustl.edu>
2022-11-14 10:26:09 -0600

    Got stuff to show up!!

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,33 +1,49 @@
 import React, { useState } from "react"
-import { CssBaseline, Card, CardMedia, CardContent, 
-	Box, Typography, Stack, Item, Container } from "@mui/material"
+import {
+  CssBaseline,
+  Card,
+  CardMedia,
+  CardContent,
+  Box,
+  Typography,
+  Stack,
+  Item,
+  Container,
+} from "@mui/material"
 import PlaylistSelector from "./PlaylistSelector"
 import IQR from "./iqrVis"
 
 //test data for vis
-const data = [
-	{avg: 0.2 },
-	{avg: -0.4 },
-	{avg: 0.6 },
-	{avg: -0.1 },
-]
+const data = [{ avg: 0.2 }, { avg: -0.4 }, { avg: 0.6 }, { avg: -0.1 }]
 
 const Dashboard = (props) => {
-	const [activePlaylist, setActivePlaylist] = useState(null)
+  const [activePlaylist, setActivePlaylist] = useState(null)
 
+  console.error("rendering dashboard")
 
-	return (
-		<React.Fragment>
-		<CssBaseline />
-		<Container>
-			<Box sx={{width: "250px", height: "64px"}} />
-			<Box sx={{display: "flex", flexGrow: 1, height: "100%", minHeight: "500px", width: "100%"}}>
-				<PlaylistSelector activePlaylist={activePlaylist} setActivePlaylist={setActivePlaylist} />
-			</Box>
-		</Container>
-		<IQR data={data} />
-		</React.Fragment>
-	)
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <Box sx={{ width: "250px", height: "64px" }} />
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            height: "100%",
+            minHeight: "500px",
+            width: "100%",
+          }}
+        >
+          <PlaylistSelector
+            activePlaylist={activePlaylist}
+            setActivePlaylist={setActivePlaylist}
+          />
+        </Box>
+      </Container>
+      <IQR data={data} />
+    </React.Fragment>
+  )
 }
 
 export { Dashboard }
--- src/Components/Dashboard/iqrVis.js
+++ src/Components/Dashboard/iqrVis.js
@@ -1,69 +1,85 @@
-import { useD3 } from '../hooks/useD3';
-import React from 'react';
-import * as d3 from 'd3';
+import { useD3 } from "../hooks/useD3"
+import React from "react"
+import * as d3 from "d3"
 
 const IQR = ({ data }) => {
+  const renderChart = (svg) => {
+    const margin = { top: 40, right: 0, bottom: 60, left: 60 }
 
-const ref = useD3(
-    (svg) => {
+    const width = 800
+    const height = 300
 
-        const margin = { top: 40, right: 0, bottom: 60, left: 60 };
-    
-        const width = 800;
-        const height = 300;
+    svg.select(".plot-area").append("text").text("TEST TEST TEST from vis")
 
-        svg.select(".plot-area").append("text").text("TEST TEST TEST from vis");
-    
-      // SVG drawing area
-        // vis.svg = d3.select("#" + vis.parentElement).append("svg")
-        //     .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        //     .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-        //     .attr("class","nlpChart");
-    
-        // //text g
-        // vis.svg.append("g").attr("class","textContainer");
-    
-        // //line container
-        // vis.svg.append("g").attr("class","lineContainer");
-    
-        // //circle container
-        // vis.svg.append("g").attr("class","circleContainer");
-    
-        //circle avg label container
-        svg.select(".plot-area").append("text").attr("class","avgLabel").style("opacity",0).attr("x",'50%').attr("y",50);
-        
-        // Scales and axes
-        const x = d3.scaleLinear()
-            .range([0, width])
-            .domain((function(d) { return d.low; }, function(d) { return d.high; }));
+    // SVG drawing area
+    // vis.svg = d3.select("#" + vis.parentElement).append("svg")
+    //     .attr("width", vis.width + vis.margin.left + vis.margin.right)
+    //     .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+    //     .attr("class","nlpChart");
 
-    
-         let circle = svg.select(".plot-area").selectAll("circle").data(data);
-    
+    // //text g
+    // vis.svg.append("g").attr("class","textContainer");
+
+    // //line container
+    // vis.svg.append("g").attr("class","lineContainer");
+
+    // //circle container
+    // vis.svg.append("g").attr("class","circleContainer");
+
+    //circle avg label container
+    svg
+      .select(".plot-area")
+      .append("text")
+      .attr("class", "avgLabel")
+      .style("opacity", 0)
+      .attr("x", "50%")
+      .attr("y", 50)
+
+    // Scales and axes
+    const x = d3
+      .scaleLinear()
+      .range([0, width])
+      .domain(
+        (function (d) {
+          return d.low
+        },
+        function (d) {
+          return d.high
+        })
+      )
+
+    let circle = svg.select(".plot-area").selectAll("circle").data(data)
+
     //Draw circles for average value
-        circle.enter().append("circle")
-            .attr("cx", function(d) { return width/2 + (100*d.avg); })
-            .attr("cy", function(d,i) { return 30 + 30*i; })
-            .merge(circle)
-            .attr("r", 10)
-            .style("fill","blue")
-            .style("opacity",.6)
-            .attr("class", "avgCircle");
-            // .on('mouseover', function (event,d) {
-            //     let label = d3.select("text.avgLabel");
-            //     label
-            //     .style("opacity",.8)
-            //     .style("fill","blue")
-            //     .attr("x",d3.select(this)._groups[0][0].cx.baseVal.value -8)
-            //     .attr("y",d3.select(this)._groups[0][0].cy.baseVal.value - 15)
-            //     .style("font-size",10)
-            //     .text(d.avg);
-            // })
-            // .on('mouseout', function (d, i) {
-            //     let label = d3.select("text.avgLabel");
-            //     label.style("opacity",0);
-            // });
-            
+    circle
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return width / 2 + 100 * d.avg
+      })
+      .attr("cy", function (d, i) {
+        return 30 + 30 * i
+      })
+      .merge(circle)
+      .attr("r", 10)
+      .style("fill", "blue")
+      .style("opacity", 0.6)
+      .attr("class", "avgCircle")
+    // .on('mouseover', function (event,d) {
+    //     let label = d3.select("text.avgLabel");
+    //     label
+    //     .style("opacity",.8)
+    //     .style("fill","blue")
+    //     .attr("x",d3.select(this)._groups[0][0].cx.baseVal.value -8)
+    //     .attr("y",d3.select(this)._groups[0][0].cy.baseVal.value - 15)
+    //     .style("font-size",10)
+    //     .text(d.avg);
+    // })
+    // .on('mouseout', function (d, i) {
+    //     let label = d3.select("text.avgLabel");
+    //     label.style("opacity",0);
+    // });
+
     // //draw circles for low value
     //         circle.enter().append("circle")
     //         .attr("cx", function(d) { return vis.width/2 + (100*d.low); })
@@ -75,7 +91,7 @@ const ref = useD3(
     //         .attr("class", "lowCircle")
     //         .on("click",function(){
     //             updateWordCloud(d3.select(this)._groups[0][0].__data__,"neg");
-    
+
     //         })
     //         .on('mouseover', function (event,d) {
     //             let label = d3.select("text.avgLabel");
@@ -91,7 +107,7 @@ const ref = useD3(
     //             let label = d3.select("text.avgLabel");
     //             label.style("opacity",0);
     //         });
-    
+
     // //draw circles for high value
     //         circle.enter().append("circle")
     //         .attr("cx", function(d) { return vis.width/2 + Math.abs(100*d.high); })
@@ -104,7 +120,7 @@ const ref = useD3(
     //         .on("click",function(){
     //             console.log(d3.select(this)._groups[0][0].__data__.posText);
     //             updateWordCloud(d3.select(this)._groups[0][0].__data__,"pos");
-    
+
     //         })
     //         .on('mouseover', function (event,d) {
     //             let label = d3.select("text.avgLabel");
@@ -120,12 +136,12 @@ const ref = useD3(
     //             let label = d3.select("text.avgLabel");
     //             label.style("opacity",0);
     //         });
-        
+
     //     // Exit
-        circle.exit().remove();
-    
+    circle.exit().remove()
+
     //     let iqrLine = d3.select("svg.nlpChart").select("g.lineContainer").selectAll("line").data(data);
-    
+
     // //draw line connecting circles by hori plane
     //     iqrLine.enter().append("line")
     //     .attr('x1', function(d){ return vis.width/2 - Math.abs(100*d.low)})
@@ -137,9 +153,9 @@ const ref = useD3(
     //     .attr("stroke-width", 2)
     //     .style("stroke-opacity", 0.5)
     //     .attr("fill", "none");
-        
+
     //     iqrLine.exit().remove();
-    
+
     // //draw avg line down middle
     //     d3.select("svg.nlpChart").append('line')
     //     .attr('x1', function(d){ return vis.width/2})
@@ -150,7 +166,7 @@ const ref = useD3(
     //     .attr("stroke-width", 2)
     //     .attr("stroke-dasharray",5.5)
     //     .attr("fill", "none");
-    
+
     // //middle line label
     //     d3.select("svg.nlpChart").append("text")
     //     .attr("x", vis.width/2)
@@ -158,10 +174,9 @@ const ref = useD3(
     //     .attr("transform", "translate(-50, -50)")
     //     .style("font-size",10)
     //     .text("Average Sentiment Score");
-    
-    
+
     //   let text = d3.select("svg.nlpChart").select("g.textContainer").selectAll("text").data(data);
-    
+
     // //add story titles
     //   text.enter().append("text")
     //   .attr("x", 0)
@@ -170,13 +185,15 @@ const ref = useD3(
     //   .merge(text)
     //   .style("font-size",10)
     //   .style("font-weight","bold");
-    
+
     //   text.exit().remove();
 
-//data length is used as a trigger to re render chartRenderFn when length of data changes
-        },[data.length]
-    );
+    //data length is used as a trigger to re render chartRenderFn when length of data changes
+  }
 
+  const ref = useD3(renderChart, [data.length])
+
+  console.log("ref:", ref)
 
   return (
     <svg
@@ -186,13 +203,14 @@ const ref = useD3(
         marginRight: "0px",
         marginLeft: "0px",
       }}
+      ref={ref}
     >
       <g className="plot-area" />
       <g className="text-area" />
       <g className="line-area" />
       <g className="circle-area" />
     </svg>
-  );
+  )
 }
 
-export default IQR;
+export default IQR
