0927f506ae2820a03c6e455b58249fcf8efabf21
Scott Massey <scott.massey@wustl.edu>
2022-11-28 23:55:36 -0600

    quick fix

--- src/Components/Dashboard/radarChart.js
+++ src/Components/Dashboard/radarChart.js
@@ -51,7 +51,7 @@ const Radar = ({ featuresSummary, selectedTrack, loading }) => {
       h: radarHeight,
       margin,
       levels: 5, //How many levels or inner circles should there be drawn
-      maxValue: 3, //What is the value that the biggest circle will represent
+      maxValue: 1, //What is the value that the biggest circle will represent
       labelFactor: 1.25, //How much farther than the radius of the outer circle should the labels be placed
       wrapWidth: 60, //The number of pixels after which a label needs to be given a new line
       opacityArea: 0.35, //The opacity of the area of the blob
