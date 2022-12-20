3ef54332dacd0e73d09a008be5d5b946a6c7fc17
robdiorio34 <robdiorio34@gmail.com>
2022-12-19 20:18:23 -0500

    video demo added

--- src/Components/VideoDemo.js
+++ src/Components/VideoDemo.js
@@ -1,5 +1,19 @@
+import React from "react"
+import { Box } from "@mui/material"
+
 const VideoDemo = (props) => {
-  return <div>Video Demo: https://youtu.be/Oidmbatsra0</div>
+  return (
+    <React.Fragment>
+      <Box
+        sx={{ paddingLeft: "82px", paddingRight: "32px", paddingTop: "60px" }}>
+        <div>
+        <a target="_blank" href="https://youtu.be/Oidmbatsra0">Video Demo</a>
+        </div>
+
+      </Box>
+    </React.Fragment>
+    
+  )
 }
 
 export { VideoDemo }
